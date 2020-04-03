4740e424f09136e53888c7e1bf354905ea4d39d2
RenoBudigaddd <kartoplyk@gmail.com>
2020-04-03 18:27:57 +0300

    create arrays for digits and actions

---
+++ app/src/main/java/com/example/java_calculator/CalculatorModel.java
@@ -0,0 +1,5 @@
+package com.example.java_calculator;
+
+public class CalculatorModel {
+    
+}
--- app/src/main/java/com/example/java_calculator/MainActivity.java
+++ app/src/main/java/com/example/java_calculator/MainActivity.java
@@ -9,5 +9,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] numberIds = new int[]
+                {
+                        R.id.zero,
+                        R.id.one,
+                        R.id.two,
+                        R.id.three,
+                        R.id.four,
+                        R.id.five,
+                        R.id.six,
+                        R.id.seven,
+                        R.id.eight,
+                        R.id.nine
+                };
+        int[] actionIds = new int[] {
+          R.id.plus,
+          R.id.minus,
+          R.id.multiply,
+          R.id.division,
+        };
     }
 }
